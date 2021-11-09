244155e5611ea9f9867ab6b69a1f68977d7f9720
Wissdx <wissem.derghal@gmail.com>
2021-11-09 13:58:59 +0100

    Test #3 | 09_11_2021
    Ajout du menu de navigation via php et css

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Voici le site web du projet de BOURLARD Tom, DERGHAL Wissem, EA Anaelle">
+    <meta name="description" content="Voici le site web du projet annuel 1A2 de BOURLARD Tom, DERGHAL Wissem, EA Anaelle">
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="assets\img\20 sur 20.png" type="image/x-icon">
-    <title>Projet annuel - 1A2 Groupe X</title>
+    <title>leunna tejorp - 1A2 Groupe X</title>
 </head>
 <body>
 
     <header>
-        <h1>Bienvenue sur le projet annuel du groupe x</h1>
+        <?php require('pages/menu.html'); ?>
+        <h1>Bienvenue sur le leunna tejorp du groupe x</h1>
     </header>
 
     <section>
