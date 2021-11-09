40624fd76d80d7c9d21bb0986d0a90853681a847
Wissdx <wissem.derghal@gmail.com>
2021-11-09 11:17:45 +0100

    Test #1 | 09_11_2021
    Premier commit avec l'organistion des dossiers et quelques fichiers basiques (potentiellement provisoire)

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Voici le site web du projet de BOURLARD Tom, DERGHAL Wissem, EA Anaelle">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="assets\img\20 sur 20.png" type="image/x-icon">
+    <title>Projet annuel - 1A2 Groupe X</title>
+</head>
+<body>
+
+    <header>
+        <h1>Bienvenue sur le projet annuel du groupe x</h1>
+    </header>
+
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur repellat unde officia voluptatum omnis, quisquam, est iure blanditiis sunt dicta sed voluptate aut necessitatibus adipisci eos, quas numquam qui illum.</p>
+    </section>
+
+    <footer>
+        <p>Copyright © ESGI 2021-2022</p>
+    </footer>
+
+</body>
+</html>
