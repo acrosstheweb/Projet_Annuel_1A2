8d41df6e14a5cf1eb4c6399162431318bf0be2b1
Tom-Brd <bourlard.tom.94@gmail.com>
2022-04-05 17:30:16 +0200

    update font loupe

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta name="description" content=<?php if(isset($content)){echo $content;}else{echo 'Fitness Essential';} ?>>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title><?php if(isset($title)){echo $title;}else{echo 'Fitness Essential';} ?></title>
 </head>
 <body>
@@ -27,7 +28,7 @@
             <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link <?php if(isset($currentPage)){isActive($currentPage,"index");} ?>" aria-current="page" href="index.php">Accueil</a>
+                        <a class="nav-link <?php if(isset($currentPage)){isActive($currentPage,"index");} ?>" href="index.php">Accueil</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link <?php if(isset($currentPage)){isActive($currentPage,"about");} ?>" href="about.php">À propos</a>
@@ -53,7 +54,7 @@
                     <div class="input-group">
                         <input type="search" class="form-control" placeholder="Rechercher" aria-label="Rechercher">
                         <button class="btn btn-outline-success" type="submit">
-                        <span class="material-icons-outlined">search</span>
+                        <span class="material-icons">search</span>
                         </button>
                     </div>
                 </form>
