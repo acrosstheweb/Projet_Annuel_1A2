c2384c7d6113bd5926416bf9ad60ccd0a2c89306
Tom-Brd <bourlard.tom.94@gmail.com>
2022-04-05 16:07:26 +0200

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta name="description" content=<?php if(isset($content)){echo $content;}else{echo "Bienvenue sur Fitness Essential";} ?>>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <title><?php if(isset($title){echo $title;};else{echo "Fitness Essential";}) ?></title>
+    <title><?php if(isset($title)){echo $title;}else{echo "Fitness Essential";} ?></title>
 </head>
 <body>
 
