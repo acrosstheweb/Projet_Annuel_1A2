2cffdb22cc3c594661e4e9c9e56ed84db54b500e
Wissdx <wissem.derghal@gmail.com>
2022-04-12 14:17:11 +0200

    Refaisage modal js

--- js/modals.js
+++ js/modals.js
@@ -1,30 +1,33 @@
-const login_trigger = document.getElementById("login-trigger");
-const close_login_modal = document.getElementById("close-login-modal");
+const login_trigger = document.getElementById("__login-trigger");
+const close_login_modal = document.getElementById("__close-login-modal");
 
-const register_trigger = document.getElementById("register-trigger");
-const close_register_modal = document.getElementById("close-register-modal");
+const register_trigger = document.getElementById("__register-trigger");
+const close_register_modal = document.getElementById("__close-register-modal");
 
-const login_modal = document.getElementById("login-modal");
-const register_modal = document.getElementById("register-modal");
+const login_modal = document.getElementById("__login-modal");
+const register_modal = document.getElementById("__register-modal");
 
-function closeLoginModal(){
-    if(login_modal.style.display == 'block'){
-        login_modal.style.display = 'none';
-    }else{
-        login_modal.style.display='block';
+login_trigger.addEventListener("click", function () {
+    if (login_modal.style.display === "none") {
+        login_modal.style.display = "block";
+    } else {
+        login_modal.style.display = "none";
     }
-}
+});
 
-function closeRegisterModal(){
-    if(register_modal.style.display == 'block'){
-        register_modal.style.display = 'none';
-    }else{
-        register_modal.style.display='block';
+
+close_login_modal.addEventListener("click", function () {
+    login_modal.style.display = "none";
+});
+
+register_trigger.addEventListener("click", function () {
+    if (register_modal.style.display === "none") {
+        register_modal.style.display = "block";
+    } else {
+        register_modal.style.display = "none";
     }
-}
+});
 
-login_trigger.addEventListener("click", closeLoginModal);
-close_login_modal.addEventListener("click", closeLoginModal);
-
-register_trigger.addEventListener("click", closeRegisterModal);
-close_register_modal.addEventListener("click", closeRegisterModal);
+close_register_modal.addEventListener("click", function () {
+    register_modal.style.display = "none";
+});
