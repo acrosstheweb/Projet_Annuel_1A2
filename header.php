5ed8d759c978d83aba50aa44cc95d8f3ff629e9f
Anaelle <aea@myges.fr>
2022-05-05 11:18:35 +0200

    Update Carousels

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/17a81231c9.js" crossorigin="anonymous"></script>
     <title><?php if (isset($title)) {
                 echo $title;
             } else {
