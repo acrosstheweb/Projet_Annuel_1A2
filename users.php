e2ed7c65f1092082b58226410ba6d2a22ff706f4
Wissdx <62649911+Wissdx@users.noreply.github.com>
2022-05-14 17:33:17 +0200

    users back-office
    JS :
         -  Factorisation du traitement modif + delete dans un seul fichier admin-users.js
    PHP
         - Gestion de la suppression d'un utilisateur via le back office

--- users.php
+++ users.php
@@ -7,20 +7,21 @@ require 'functions.php';
     }
 
 require 'header.php';
+Message('Delete');
 ?>
 
 <h1 class="aligned-title">Liste des utilisateurs</h1>
 
 <div class="row d-flex justify-content-center">
     <div class="col-10">
-	<table class="table">
+	<table class="table" id="usersTable">
 		<thead>
 			<tr>
-				<th>Id</th>
-				<th>Email</th>
-				<th>Nom</th>
-				<th>Prénom</th>
-				<th>Date de naissance</th>
+				<th onclick="sortColumn(0, 'usersTable')" id="tableHeaderSortable">Id</th>
+				<th onclick="sortColumn(1, 'usersTable')" id="tableHeaderSortable">Email</th>
+				<th onclick="sortColumn(2, 'usersTable')" id="tableHeaderSortable">Nom</th>
+				<th onclick="sortColumn(3, 'usersTable')" id="tableHeaderSortable">Prénom</th>
+				<th onclick="sortColumn(4, 'usersTable')" id="tableHeaderSortable">Date de naissance</th>
 				<th>Action</th>
 			</tr>
 		</thead>
@@ -59,106 +60,108 @@ require 'header.php';
 
 						</tr>
 
-						<div class="modal fade" id="delModalUid<?php echo $userId;?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					    <div class="modal fade" id="modifyModalUid<?php echo $userId;?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 							<div class="modal-dialog">
 								<div class="modal-content">
-								<div class="modal-header">
-									<h5 class="modal-title" id="exampleModalLabel">Suppression d'un utilisateur</h5>
-									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-								</div>
-								<div class="modal-body">
-									Vous êtes sur le point de supprimer l'utilisateur suivant:
-									<div class="row">
-										<p class="col"><strong>Nom</strong><br><?php echo $userLastName;?></p>
-										<p class="col"><strong>Prénom</strong><br><?php echo $userFirstName;?></p>
-									</div>
-									<div class="row">
-										<p><strong>Adresse e-mail</strong><br><?php echo $userMail;?></p>
-									</div>
-									<p id="delete-passwordConfirmDescription">Êtes-vous sûr de vouloir le supprimer?</p>
-									
-									<div class="row" id="delete-adminPassword">
-										<div class="col">
-											<label for="delete-adminPasswordInput" class="fw-bold">Mot de passe Administrateur </label>
-											<input id="delete-adminPasswordInput" class="form-control" type="email" name="delete-adminPasswordInput" placeholder="Veuillez saisir votre mot de passe" required="required">
-										</div>
-									</div>
-								</div>
-								<div class="modal-footer">
-									<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-									<a href="#" class="btn btn-primary" id="delete-passwordConfirm">Supprimer</a>
-									<a href="userDel.php?id=<?php echo $userId;?>" class="btn btn-primary" id="delete-confirm">Supprimer</a>
-								</div>
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Modification des informations de l'utilisateur</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Vous êtes sur le point de modifier les informations de l'utilisateur suivant:
+                                        <form id="modifyUserFormUid<?php echo $userId;?>" action="userModify.php?id=<?php echo $userId;?>" method="POST">
+                                            <div class="modifyFormInfo">
+                                                <div class="row mt-3">
+                                                    <div class="col-6">
+                                                        <label for="modify-lastNameUid<?php echo $userId;?>" class="fw-bold">Nom </label>
+                                                        <input id="modify-lastNameUid<?php echo $userId;?>" class="form-control" type="text" name="modify-lastNameUid<?php echo $userId;?>" value="<?php echo $userLastName;?>" required="required">
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <label for="modify-firstNameUid<?php echo $userId;?>" class="fw-bold">Prénom </label>
+                                                        <input id="modify-firstNameUid<?php echo $userId;?>" class="form-control" type="text" name="modify-firstNameUid<?php echo $userId;?>" value="<?php echo $userFirstName;?>" required="required">
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-6">
+                                                        <label for="modify-birthdayUid<?php echo $userId;?>" class="fw-bold">Date de naissance </label>
+                                                        <input id="modify-birthdayUid<?php echo $userId;?>" class="form-control" type="date" name="modify-birthdayUid<?php echo $userId;?>" value="<?php echo $userBirthday;?>" required="required">
+                                                    </div>
+                                                </div>
+                                                <div class="row mt-3">
+                                                    <div class="col">
+                                                        <label for="modify-emailUid<?php echo $userId;?>" class="fw-bold">Adresse e-mail </label>
+                                                        <input id="modify-emailUid<?php echo $userId;?>" class="form-control" type="email" name="modify-emailUid<?php echo $userId;?>" value="<?php echo $userMail;?>" required="required">
+                                                    </div>
+                                                </div>
+                                                <div class="row mt-3">
+                                                    <div class="col">
+                                                        <label for="modify-addressUid<?php echo $userId;?>" class="fw-bold">Adresse </label>
+                                                        <input id="modify-addressUid<?php echo $userId;?>" class="form-control" type="text" name="modify-addressUid<?php echo $userId;?>" value="<?php echo $userAddress;?>" required="required">
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-6">
+                                                        <label for="modify-zipCodeUid<?php echo $userId;?>" class="fw-bold">Code postal </label>
+                                                        <input id="modify-zipCodeUid<?php echo $userId;?>" class="form-control" type="text" name="modify-zipCodeUid<?php echo $userId;?>" value="<?php echo $userZipCode;?>" required="required">
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <label for="modify-cityUid<?php echo $userId;?>" class="fw-bold">Ville </label>
+                                                        <input id="modify-cityUid<?php echo $userId;?>" class="form-control" type="text" name="modify-cityUid<?php echo $userId;?>" value="<?php echo $userCity;?>" required="required">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row mt-3 modify-adminPassword">
+                                                <div class="col">
+                                                    <label for="modify-adminPasswordInputUid<?php echo $userId;?>" class="fw-bold">Mot de passe Administrateur </label>
+                                                    <input id="modify-adminPasswordInputUid<?php echo $userId;?>" class="form-control" type="password" name="modify-adminPasswordInputUid<?php echo $userId;?>" placeholder="Veuillez saisir votre mot de passe" required="required">
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                                        <button class="btn btn-primary modify-passwordConfirm">Modifier</button>
+                                        <button class="btn btn-primary modify-confirm" form="modifyUserFormUid<?php echo $userId;?>" type="submit">Modifier</button>
+                                    </div>
 								</div>
 							</div>
 						</div>
 
-						<div class="modal fade" id="modifyModalUid<?php echo $userId;?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-							<div class="modal-dialog">
-								<div class="modal-content">
-								<div class="modal-header">
-									<h5 class="modal-title" id="exampleModalLabel">Modification des informations de l'utilisateur</h5>
-									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-								</div>
-								<div class="modal-body">
-									Vous êtes sur le point de modifier les informations de l'utilisateur suivant:
-									<form id="modify-formUid<?php echo $userId;?>" action="userModify.php?id=<?php echo $userId;?>" method="POST">
-										<div class="modifyFormInfo">
-											<div class="row mt-3">
-												<div class="col-6">
-													<label for="modify-lastNameUid<?php echo $userId;?>" class="fw-bold">Nom </label>
-													<input id="modify-lastNameUid<?php echo $userId;?>" class="form-control" type="text" name="modify-lastNameUid<?php echo $userId;?>" value="<?php echo $userLastName;?>" required="required">
-												</div>
-												<div class="col-6">
-													<label for="modify-firstNameUid<?php echo $userId;?>" class="fw-bold">Prénom </label>
-													<input id="modify-firstNameUid<?php echo $userId;?>" class="form-control" type="text" name="modify-firstNameUid<?php echo $userId;?>" value="<?php echo $userFirstName;?>" required="required">
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-6">
-													<label for="modify-birthdayUid<?php echo $userId;?>" class="fw-bold">Date de naissance </label>
-													<input id="modify-birthdayUid<?php echo $userId;?>" class="form-control" type="date" name="modify-birthdayUid<?php echo $userId;?>" value="<?php echo $userBirthday;?>" required="required">
-												</div>
-											</div>
-											<div class="row mt-3">
-												<div class="col">
-													<label for="modify-emailUid<?php echo $userId;?>" class="fw-bold">Adresse e-mail </label>
-													<input id="modify-emailUid<?php echo $userId;?>" class="form-control" type="email" name="modify-emailUid<?php echo $userId;?>" value="<?php echo $userMail;?>" required="required">
-												</div>
-											</div>
-											<div class="row mt-3">
-												<div class="col">
-													<label for="modify-addressUid<?php echo $userId;?>" class="fw-bold">Adresse </label>
-													<input id="modify-addressUid<?php echo $userId;?>" class="form-control" type="text" name="modify-addressUid<?php echo $userId;?>" value="<?php echo $userAddress;?>" required="required">
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-6">
-													<label for="modify-zipCodeUid<?php echo $userId;?>" class="fw-bold">Code postal </label>
-													<input id="modify-zipCodeUid<?php echo $userId;?>" class="form-control" type="text" name="modify-zipCodeUid<?php echo $userId;?>" value="<?php echo $userZipCode;?>" required="required">
-												</div>
-												<div class="col-6">
-													<label for="modify-cityUid<?php echo $userId;?>" class="fw-bold">Ville </label>
-													<input id="modify-cityUid<?php echo $userId;?>" class="form-control" type="text" name="modify-cityUid<?php echo $userId;?>" value="<?php echo $userCity;?>" required="required">
-												</div>
-											</div>
-										</div>
-										<div class="row mt-3 modify-adminPassword">
-											<div class="col">
-												<label for="modify-adminPasswordInputUid<?php echo $userId;?>" class="fw-bold">Mot de passe Administrateur </label>
-												<input id="modify-adminPasswordInputUid<?php echo $userId;?>" class="form-control" type="email" name="modify-adminPasswordInputUid<?php echo $userId;?>" placeholder="Veuillez saisir votre mot de passe" required="required">
-											</div>
-										</div>
-									</form>
-								</div>
-								<div class="modal-footer">
-									<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-									<a href="#" class="btn btn-primary modify-passwordConfirm">Modifier</a>
-									<button class="btn btn-primary modify-confirm" form="modify-formUid<?php echo $userId;?>" type="submit">Modifier</button>
-								</div>
-								</div>
-							</div>
-						</div>
+                        <div class="modal fade" id="delModalUid<?php echo $userId;?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Suppression d'un utilisateur</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form id="deleteUserFormUid<?php echo $userId;?>" action="userDel.php?id=<?php echo $userId;?>" method="POST" >
+                                            <h5>Vous êtes sur le point de supprimer l'utilisateur suivant:</h5>
+                                            <div class="row">
+                                                <p class="col"><strong>Nom</strong><br><?php echo $userLastName;?></p>
+                                                <p class="col"><strong>Prénom</strong><br><?php echo $userFirstName;?></p>
+                                            </div>
+                                            <div class="row">
+                                                <p><strong>Adresse e-mail</strong><br><?php echo $userMail;?></p>
+                                            </div>
+                                            <p class="delete-passwordConfirmDescription">Êtes-vous sûr de vouloir le supprimer?</p>
+
+                                            <div class="row delete-adminPassword">
+                                                <div class="col">
+                                                    <label for="delete-adminPasswordInput" class="fw-bold">Mot de passe Administrateur </label>
+                                                    <input id="delete-adminPasswordInput" class="form-control" type="password" name="delete-adminPasswordInput" placeholder="Veuillez saisir votre mot de passe" required="required">
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                                        <button class="btn btn-primary delete-passwordConfirm">Supprimer</button>
+                                        <button class="btn btn-primary delete-confirm" form="deleteUserFormUid<?php echo $userId;?>" type="submit">Supprimer</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
 					<?php
 				}
@@ -176,5 +179,5 @@ include "footer.php";
 
 ?>
 
-<script src="js/modifyUserAdmin.js" crossorigin="anonymous"></script>
-<script src="js/deleteUserAdmin.js" crossorigin="anonymous"></script>
+
+<script src="js/admin-users.js" crossorigin="anonymous"></script>
