7c6c683e7c1fcdd4529e87d8023fb3fb8d81a92d
Tom-Brd <bourlard.tom.94@gmail.com>
2022-05-16 23:57:11 +0200

    Update
    update forum.php + creation question.php
    SQL request to display topics and questions which are in the bdd

--- forum.php
+++ forum.php
@@ -4,11 +4,46 @@
     $currentPage = 'forum';
     require 'header.php';
     Message('UploadImage');
+    Message('No Topic');
+    
+    $pdo = database();
+
+    $req = $pdo->query("SELECT * FROM RkU_TOPIC ORDER BY topicOrder");
+
+    $results = $req->fetchAll();
 ?>
 
 <h2 class="aligned-title"> Forum de Fitness Essential </h2>
 
-<div class="row">
+        <div class="container">
+            <div class="row">
+
+<?php
+    foreach($results as $categorie){
+?>
+
+                <div class="col-sm-12 col-md-12 col-lg-6">
+                    <div class="card" style="width: 30rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $categorie['title'] ?></h5>
+                            <p class="card-text"><?php echo $categorie['description'] ?></p>
+                            <a href="question.php?id=<?php echo $categorie['id'] ?>" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>        
+                
+                
+                
+                
+<?php
+} 
+?>
+
+            </div>
+        </div>
+
+<!-- <div class="row">
     <form action="uploadImage.php" method = "POST" enctype ="multipart/form-data"> // Le enctype permet de spécifier que les données envoyées lors de l'envoie sont encodées lors de la soumission au serveur.
         <div class="col-4 mb-2">
             <label for="file" class="form-label">Veuillez insérer votre image</label>
@@ -18,7 +53,7 @@
             <button class="btn btn-primary" type="submit">Submit form</button>
         </div>
     </form>
-</div>
+</div> -->
 
 
 <?php
