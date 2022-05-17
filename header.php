a22609eb82e815996f64d2255dbbdbf14083ada6
Anaelle <aea@myges.fr>
2022-05-17 09:36:34 +0200

    Update logo icon

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
                                         } else {
                                             echo "Bienvenue sur Fitness Essential";
                                         } ?>>
-    <link rel="icon" type="image/x-icon" href="sources/img/logo.png">
+    <link rel="icon" type="image/x-icon" href="sources/img/icon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
@@ -45,7 +45,7 @@ flex, par défaut -> 'justify-content: space-between'  -->
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container-fluid">
                 <a href="index.php" class="navbar-brand">
-                    <img src="sources/img/logo.png" alt="logo">
+                    <img src="sources/img/icon.png" alt="logo" class="img-fluid">
                 </a>
 
                 <!-- Création du burger menu  -->
