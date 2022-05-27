b3a4caeb181b91087040690bf801e85f3d733432
Anaelle <aea@myges.fr>
2022-05-27 23:49:57 +0200

    Update programs.php

--- programs.php
+++ programs.php
@@ -7,6 +7,266 @@
 
 <h1 class="aligned-title"> Qui que vous soyez, nous vous proposons des programmes adaptés à vos envies </h1>
 
+<div class="container-fluid">
+    <div class="row d-flex justify-content-around">
+
+        <div class="card bg-dark text-white col-10 col-md-5 col-lg-3 text-center p-0 __programCard">
+            <img src="sources/img/pull1.jpg" class="card-img __programImage" alt="pull1">
+            <div class="card-img-overlay">
+                <div class="__programDescription">
+                    <div class="__cardDescriptionText p-4">
+                        <h5 class="card-title">PULL #1</h5>
+                        <table class="table text-light card-text __programContent">
+                            <tbody>
+                                <tr>
+                                    <td>Tirage vertical pronation</td>
+                                    <td>4X12</td>
+                                </tr>
+                                <tr>
+                                    <td>Rowing barre</td>
+                                    <td>4X15</td>
+                                </tr>
+                                <tr>
+                                    <td>Rowing unilateral</td>
+                                    <td>3X12</td>
+                                </tr>
+                                <tr>
+                                    <td>Tirage vertical supination</td>
+                                    <td>4X8</td>
+                                </tr>
+                                <tr>
+                                    <td>Curl barre droite</td>
+                                    <td>4X10</td>
+                                </tr>
+                                <tr>
+                                    <td>Curl marteau</td>
+                                    <td>4X12</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <a href="#" id="__seeMorePull1" class="btn btn-primary __programControl">Voir plus</a>
+                        <a href="#" id="__seeLessPull1" class="btn btn-primary __programControl">Voir moins</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card bg-dark text-white col-10 col-md-5 col-lg-3 text-center p-0 __programCard">
+            <img src="sources/img/push1.jpg" class="card-img __programImage" alt="push1">
+            <div class="card-img-overlay">
+                <div class="__programDescription">
+                    <div class="__cardDescriptionText p-4">
+                        <h5 class="card-title">PUSH #1</h5>
+                        <table class="table text-light card-text __programContent">
+                            <tbody>
+                                <tr>
+                                    <td>Développé couché</td>
+                                    <td>4X12</td>
+                                </tr>
+                                <tr>
+                                    <td>Développé incliné</td>
+                                    <td>4X12</td>
+                                </tr>
+                                <tr>
+                                    <td>Développé militaire</td>
+                                    <td>4X10</td>
+                                </tr>
+                                <tr>
+                                    <td>Tirage menton</td>
+                                    <td>3X15</td>
+                                </tr>
+                                <tr>
+                                    <td>Élévation latérale</td>
+                                    <td>4X15</td>
+                                </tr>
+                                <tr>
+                                    <td>Oiseau poulie</td>
+                                    <td>4X15</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <a href="#" id="__seeMorePush1" class="btn btn-primary __programControl">Voir plus</a>
+                        <a href="#" id="__seeLessPush1" class="btn btn-primary __programControl">Voir moins</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card bg-dark text-white col-10 col-md-5 col-lg-3 text-center p-0 __programCard">
+            <img src="sources/img/legs1.jpg" class="card-img __programImage" alt="legs1">
+            <div class="card-img-overlay">
+                <div class="__programDescription">
+                    <div class="__cardDescriptionText p-4">
+                        <h5 class="card-title">LEGS #1</h5>
+                        <table class="table text-light card-text __programContent">
+                            <tbody>
+                                <tr>
+                                    <td>Leg curl</td>
+                                    <td>4X20</td>
+                                </tr>
+                                <tr>
+                                    <td>Low squat</td>
+                                    <td>4X20</td>
+                                </tr>
+                                <tr>
+                                    <td>Presse inclinée</td>
+                                    <td>4X10</td>
+                                </tr>
+                                <tr>
+                                    <td>Sdt jambes tendues</td>
+                                    <td>4X10</td>
+                                </tr>
+                                <tr>
+                                    <td>Hack squat inversé</td>
+                                    <td>4X12</td>
+                                </tr>
+                                <tr>
+                                    <td>Mollet</td>
+                                    <td>4X10</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <a href="#" id="__seeMoreLegs1" class="btn btn-primary __programControl">Voir plus</a>
+                        <a href="#" id="__seeLessLegs1" class="btn btn-primary __programControl">Voir moins</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+<!-- 
+    </div>
+
+    <div class="row d-flex justify-content-around"> -->
+
+        <div class="card bg-dark text-white col-10 col-md-5 col-lg-3 text-center p-0 __programCard">
+            <img src="sources/img/pull2.jpg" class="card-img __programImage" alt="pull2">
+            <div class="card-img-overlay">
+                <div class="__programDescription">
+                    <div class="__cardDescriptionText p-4">
+                        <h5 class="card-title">PULL #2</h5>
+                        <table class="table text-light card-text __programContent">
+                            <tbody>
+                                <tr>
+                                    <td>Soulevé de terre convergent</td>
+                                    <td>5X5</td>
+                                </tr>
+                                <tr>
+                                    <td>Tirage horizontal</td>
+                                    <td>4X8</td>
+                                </tr>
+                                <tr>
+                                    <td>Face pull</td>
+                                    <td>4X15</td>
+                                </tr>
+                                <tr>
+                                    <td>Lat pulldown</td>
+                                    <td>3X15</td>
+                                </tr>
+                                <tr>
+                                    <td>Tirage vertical serré</td>
+                                    <td>4X8</td>
+                                </tr>
+                                <tr>
+                                    <td>Curl poulie</td>
+                                    <td>4X12</td>
+                                </tr>
+                                <tr>
+                                    <td>Curl poulie corde</td>
+                                    <td>4X12</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <a href="#" id="__seeMorePull2" class="btn btn-primary __programControl">Voir plus</a>
+                        <a href="#" id="__seeLessPull2" class="btn btn-primary __programControl">Voir moins</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card bg-dark text-white col-10 col-md-5 col-lg-3 text-center p-0 __programCard">
+            <img src="sources/img/push2.jpg" class="card-img __programImage" alt="push2">
+            <div class="card-img-overlay">
+                <div class="__programDescription">
+                    <div class="__cardDescriptionText p-4">
+                        <h5 class="card-title">PUSH #2</h5>
+                        <table class="table text-light card-text __programContent">
+                            <tbody>
+                                <tr>
+                                    <td>Développé couché</td>
+                                    <td>4X10</td>
+                                </tr>
+                                <tr>
+                                    <td>Développé incliné</td>
+                                    <td>4X10</td>
+                                </tr>
+                                <tr>
+                                    <td>Écarté sur poulie</td>
+                                    <td>3X15</td>
+                                </tr>
+                                <tr>
+                                    <td>Dips</td>
+                                    <td>4X10</td>
+                                </tr>
+                                <tr>
+                                    <td>Extension triceps poulie haute</td>
+                                    <td>4X12</td>
+                                </tr>
+                                <tr>
+                                    <td>Extension triceps poulie basse</td>
+                                    <td>4X12</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <a href="#" id="__seeMorePush2" class="btn btn-primary __programControl">Voir plus</a>
+                        <a href="#" id="__seeLessPush2" class="btn btn-primary __programControl">Voir moins</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card bg-dark text-white col-10 col-md-5 col-lg-3 text-center p-0 __programCard">
+            <img src="sources/img/legs2.jpg" class="card-img" alt="legs2">
+            <div class="card-img-overlay">
+                <div class="__programDescription">
+                    <div class="__cardDescriptionText p-4">
+                        <h5 class="card-title">LEGS #2</h5>
+                        <table class="table text-light card-text __programContent">
+                            <tbody>
+                                <tr>
+                                    <td>Leg extension</td>
+                                    <td>4X20</td>
+                                </tr>
+                                <tr>
+                                    <td>Squat</td>
+                                    <td>4X10</td>
+                                </tr>
+                                <tr>
+                                    <td>Presse incliné serré</td>
+                                    <td>4X10+10</td>
+                                </tr>
+                                <tr>
+                                    <td>Fentes smith machine</td>
+                                    <td>3X10</td>
+                                </tr>
+                                <tr>
+                                    <td>Hack squat</td>
+                                    <td>4X10</td>
+                                </tr>
+                                <tr>
+                                    <td>Mollet</td>
+                                    <td>4X10-15</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <a href="#" id="__seeMoreLegs2" class="btn btn-primary __programControl">Voir plus</a>
+                        <a href="#" id="__seeLessLegs2" class="btn btn-primary __programControl">Voir moins</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
 
 <?php
     include 'footer.php';
