475b7b274475cd361c4b0fc34c6fc8a7a248c3df
Wissdx <62649911+Wissdx@users.noreply.github.com>
2022-05-30 19:56:26 +0200

    Bcp de choses

--- exportDataRGPD.php
+++ exportDataRGPD.php
@@ -10,8 +10,9 @@ $db = database();
 
 $getUserInfoQuery = $db->query("SELECT * FROM RkU_user WHERE id=$userId");
 $getUserInfo = $getUserInfoQuery->fetch();
-$lastname = $getUserInfo['lastName']; $firstname = $getUserInfo['firstName'];
-$email = $getUserInfo['email']; $address = $getUserInfo['address']; $city = $getUserInfo['city'];
+$lastname = $getUserInfo['lastName']; $firstname = $getUserInfo['firstName']; $email = $getUserInfo['email'];
+$address = $getUserInfo['address']; $city = $getUserInfo['city']; $zipcode = $getUserInfo['zipCode'];
+$registrationDate = date('d/m/Y à H:i', strtotime($getUserInfo['registrationDate'])); $fitcoins = $getUserInfo['fitcoin'];
 
 // Meta Informations du PDF
 $pdf = new FPDF('P','mm','A4'); // A4 = 210 × 297 mm
@@ -23,24 +24,34 @@ $pdf->Image('sources/img/logo.png',105-10,6,20); // 105(A4.width/2) - 10(taille 
 $pdf->Cell(15,10,"$lastname $firstname");
 $pdf->SetX(-35);
 $pdf->Cell(0,10,"RGPD");
-$pdf->SetY(50);
+$pdf->SetY(35);
+$pdf->Cell(0,10,utf8_decode("Compte crée le $registrationDate"), 0, 0 ,'C'); // utf8_decode permet d'écrire de caractères utf8
+$pdf->SetY(70);
 
 // Body
 $pdf->SetX(45);
-$pdf->Cell(50,6,'Champ', 1,0, 'C');
-$pdf->Cell(85,6,'Valeur en BDD', 1,1, 'C');
+$pdf->Cell(50,12,'Champ', 1,0, 'C');
+$pdf->Cell(85,12,'Valeur en BDD', 1,1, 'C');
 
 $pdf->SetX(45);
-$pdf->Cell(50,6,'Adresse mail', 1,0, 'C');
-$pdf->Cell(85,6,"$email", 1,1, 'C');
+$pdf->Cell(50,8,'Adresse mail', 1,0, 'C');
+$pdf->Cell(85,8,"$email", 1,1, 'C');
 
 $pdf->SetX(45);
-$pdf->Cell(50,6,'Adresse', 1,0, 'C');
-$pdf->Cell(85,6,"$address", 1,1, 'C');
+$pdf->Cell(50,8,'Adresse', 1,0, 'C');
+$pdf->Cell(85,8,"$address", 1,1, 'C');
 
 $pdf->SetX(45);
-$pdf->Cell(50,6,'Ville', 1,0, 'C');
-$pdf->Cell(85,6,"$city", 1,1, 'C');
+$pdf->Cell(50,8,'Ville', 1,0, 'C');
+$pdf->Cell(85,8,"$city", 1,1, 'C');
+
+$pdf->SetX(45);
+$pdf->Cell(50,8,'Code postal', 1,0, 'C');
+$pdf->Cell(85,8,"$zipcode", 1,1, 'C');
+
+$pdf->SetX(45);
+$pdf->Cell(50,8,'Solde fitcoins', 1,0, 'C');
+$pdf->Cell(85,8,"$fitcoins FC", 1,1, 'C');
 
 /*$pdf->SetDrawColor(183); // Couleur du fond RVB
 $pdf->SetFillColor(221); // Couleur des filets RVB
