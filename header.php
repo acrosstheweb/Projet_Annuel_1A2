067fb50046746496710b7d7ff31d5729244cda13
Tom-Brd <bourlard.tom.94@gmail.com>
2022-05-30 19:51:46 +0200

    Update header.php

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
     <link rel="icon" type="image/x-icon" href="sources/img/icon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="<?= webroot() . 'css/style.css'?>">
+    <link rel="stylesheet" href='http://localhost/GitHub/Projet_Annuel_1A2/css/style.css'>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://kit.fontawesome.com/17a81231c9.js" crossorigin="anonymous"></script>
     <title><?php if (isset($title)) {
