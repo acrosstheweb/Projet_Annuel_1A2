ff3b00c25993f67a4aadd33a846171ae421eb09e
Tom-Brd <bourlard.tom.94@gmail.com>
2022-06-23 00:35:34 +0200

    Update inscription / desinscription

--- modules/calendar/scripts/Calendar/EventDesinscription.php
+++ modules/calendar/scripts/Calendar/EventDesinscription.php
@@ -25,34 +25,38 @@ $participateReq->execute([
 ]);
 $isParticipated = $participateReq->fetch()['userId'];
 
-if($isParticipated){
-    $deleteUserFromBooking = $pdo->prepare("DELETE FROM RkU_PARTICIPATE WHERE userId=:userId AND eventId=:eventId");
-    $deleteUserFromBooking->execute([
-        'userId'=>$userId,
-        'eventId'=>$event['id']
-    ]);
-
-    $places = $pdo->prepare("UPDATE RkU_BOOKING SET places=:places WHERE id=:eventId");
-    $places->execute([
-        'eventId'=>$event['id'],
-        'places'=>$event['places']+1
-    ]);
-
-    $userWallet = $pdo->prepare("UPDATE RkU_USER SET fitcoin=:fitcoin WHERE id=:userId");
-    $userWallet->execute([
-        'fitcoin'=>$fitcoins + $event['price'],
-        'userId'=>$userId
-    ]);
-
-    setMessage('eventDesinscription', ['Vous êtes bien désinscrit'],'danger');
+if(!isConnected()){
+    setMessage('inscriptionEvent', ['Vous n\'êtes pas connecté, merci de vous connecter pour vous désinscrire (si vous vous êtes inscrit)'],'danger');
     header('Location: ../../vues/eventUser.php?id=' . $event['id']);
     die();
 }
 else{
-    setMessage('eventDesinscription', ['Vous n\'êtes pas inscrit'],'danger');
-    header('Location: ../../vues/eventUser.php?id=' . $event['id']);
-    die();
+    if($isParticipated){
+        $deleteUserFromBooking = $pdo->prepare("DELETE FROM RkU_PARTICIPATE WHERE userId=:userId AND eventId=:eventId");
+        $deleteUserFromBooking->execute([
+            'userId'=>$userId,
+            'eventId'=>$event['id']
+        ]);
+
+        $places = $pdo->prepare("UPDATE RkU_BOOKING SET places=:places WHERE id=:eventId");
+        $places->execute([
+            'eventId'=>$event['id'],
+            'places'=>$event['places']+1
+        ]);
+
+        $userWallet = $pdo->prepare("UPDATE RkU_USER SET fitcoin=:fitcoin WHERE id=:userId");
+        $userWallet->execute([
+            'fitcoin'=>$fitcoins + $event['price'],
+            'userId'=>$userId
+        ]);
+
+        setMessage('eventDesinscription', ['Vous êtes bien désinscrit'],'danger');
+        header('Location: ../../vues/eventUser.php?id=' . $event['id']);
+        die();
+    }
+    else{
+        setMessage('eventDesinscription', ['Vous n\'êtes pas inscrit'],'danger');
+        header('Location: ../../vues/eventUser.php?id=' . $event['id']);
+        die();
+    }
 }
-
-
-
--- modules/calendar/scripts/Calendar/EventInscription.php
+++ modules/calendar/scripts/Calendar/EventInscription.php
@@ -25,51 +25,59 @@ $participateReq->execute([
 ]);
 $isParticipated = $participateReq->fetch()['userId'];
 
-if($isParticipated == $userId){
-    setMessage('inscriptionEvent', ["Vous êtes déjà inscrit à cet évènement, vous pouvez vous rendre sur
-    sur <a href='../vues/reservations.php'>cette page</a> pour réserver une autre séance"], 'warning');
-    header('Location: ../../vues/eventuser.php?id=' . $event['id']);
-    exit;
+if(!isConnected()){
+    setMessage('inscriptionEvent', ['Vous n\'êtes pas connecté, merci de vous connecter pour vous inscrire'],'danger');
+    header('Location: ../../vues/eventUser.php?id=' . $event['id']);
+    die();
 }
 else{
-    if($event['price'] <= $fitcoins){
-        if($event['places'] == 0){
-            setMessage('inscriptionEvent', ["Il n'y a plus suffisamment de place pour vous inscrire, vous pouvez aller checker
-            sur <a href='../vues/reservations.php'>cette page</a> pour réserver une autre séance"], 'warning');
-            header('Location: ../../vues/eventUser.php?id=' . $event['id']);
-            exit;
-        }
-        else{
-            $places = $pdo->prepare("UPDATE RkU_BOOKING SET places=:places WHERE id=:eventId");
-            $places->execute([
-                'eventId'=>$event['id'],
-                'places'=>$event['places']-1
-            ]);
-    
-            $userWallet = $pdo->prepare("UPDATE RkU_USER SET fitcoin=:fitcoin WHERE id=:userId");
-            $userWallet->execute([
-                'fitcoin'=>$fitcoins - $event['price'],
-                'userId'=>$userId
-            ]);
-    
-            $inscriptionQuery = $pdo->prepare("INSERT INTO RkU_PARTICIPATE (userId, eventId) VALUES (:userId, :eventId)");
-            $inscriptionQuery->execute([
-                'userId'=>$userId,
-                'eventId'=>$event['id']
-            ]);
-    
-            setMessage('inscriptionEvent', ["Vous êtes bien inscrit à la séance : " . $event['name']], 'success');
-            header('Location: ../../vues/eventUser.php?id=' . $event['id']);
-            exit;
-        }
-    }
-    else{
-        setMessage('inscriptionEvent', ["Vous n'avez pas assez de fitcoins pour réserver cette séance, vous pouvez vous rendre
-        sur <a href='#'>cette page</a> pour en acheter"], 'warning');
-        header('Location: ../../vues/eventUser.php?id=' . $event['id']);
+    if($isParticipated == $userId){
+        setMessage('inscriptionEvent', ["Vous êtes déjà inscrit à cet évènement, vous pouvez vous rendre sur
+        sur <a href='../vues/reservations.php'>cette page</a> pour réserver une autre séance"], 'warning');
+        header('Location: ../../vues/eventuser.php?id=' . $event['id']);
         exit;
     }
+    else{
+        if($event['price'] <= $fitcoins){
+            if($event['places'] == 0){
+                setMessage('inscriptionEvent', ["Il n'y a plus suffisamment de place pour vous inscrire, vous pouvez aller checker
+                sur <a href='../vues/reservations.php'>cette page</a> pour réserver une autre séance"], 'warning');
+                header('Location: ../../vues/eventUser.php?id=' . $event['id']);
+                exit;
+            }
+            else{
+                $places = $pdo->prepare("UPDATE RkU_BOOKING SET places=:places WHERE id=:eventId");
+                $places->execute([
+                    'eventId'=>$event['id'],
+                    'places'=>$event['places']-1
+                ]);
+        
+                $userWallet = $pdo->prepare("UPDATE RkU_USER SET fitcoin=:fitcoin WHERE id=:userId");
+                $userWallet->execute([
+                    'fitcoin'=>$fitcoins - $event['price'],
+                    'userId'=>$userId
+                ]);
+        
+                $inscriptionQuery = $pdo->prepare("INSERT INTO RkU_PARTICIPATE (userId, eventId) VALUES (:userId, :eventId)");
+                $inscriptionQuery->execute([
+                    'userId'=>$userId,
+                    'eventId'=>$event['id']
+                ]);
+        
+                setMessage('inscriptionEvent', ["Vous êtes bien inscrit à la séance : " . $event['name']], 'success');
+                header('Location: ../../vues/eventUser.php?id=' . $event['id']);
+                exit;
+            }
+        }
+        else{
+            setMessage('inscriptionEvent', ["Vous n'avez pas assez de fitcoins pour réserver cette séance, vous pouvez vous rendre
+            sur <a href='#'>cette page</a> pour en acheter"], 'warning');
+            header('Location: ../../vues/eventUser.php?id=' . $event['id']);
+            exit;
+        }
+    }
 }
+
 
 
 
