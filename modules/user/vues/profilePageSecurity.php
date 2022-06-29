cb0a9c08aef5d7ac3eadd5085ca8a5f9332e7a7f
Anaelle <aea@myges.fr>
2022-06-29 03:15:28 +0200

    fix profile module

--- modules/user/vues/profilePageSecurity.php
+++ modules/user/vues/profilePageSecurity.php
@@ -16,7 +16,7 @@ Message('DeleteUser');
 Message('updateMail');
 Message('updatePassword');
 ?>
-<div class="container-fluid d-md-none">
+<div class="container-fluid d-lg-none">
     <div class="row __profileDropdown">
         <div class="dropdown d-grid gap-2">
             <button class="btn dropdown-toggle text-light" type="button" id="__profileDropdownButton" data-bs-toggle="dropdown" aria-expanded="false">
@@ -31,13 +31,12 @@ Message('updatePassword');
 
 <h2 class="aligned-title d-none d-md-block"> Sécurité </h2>
 <div class="container-fluid">
-    <div class="row">
-
-        <div class="d-none col-2 mx-md-3 d-md-flex justify-content-center">
-            <?php include 'profilePageNavbar.php'; ?>
+    <div class="row d-flex justify-content-center justify-content-lg-start">
+        <div class="d-none col-2 d-lg-flex justify-content-center">
+            <?php include "profilePageNavbar.php"; ?>
         </div>
-        
-        <div class="col-12 col-md-8">
+
+        <div class="col-12 col-md-10 col-lg-8">
 
             <div class="row mt-3">
                 <div class="row">
