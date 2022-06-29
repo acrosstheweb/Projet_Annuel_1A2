d9c3ee33b27f672b0739cdba45d5e08da31cad83
Wissdx <62649911+Wissdx@users.noreply.github.com>
2022-06-29 02:46:51 +0200

    CAPTCHA TEST

--- captcha_v2.php
+++ captcha_v2.php
@@ -1,6 +1,10 @@
 <?php
 require_once 'functions.php';
 
+$filePath = ABSOLUTE_PATH . "sources/captcha/tileNumber.txt";
+$fileTileNumber = fopen($filePath, 'r');
+$tileLength = (int)fread($fileTileNumber, filesize($filePath));
+fclose($fileTileNumber);
 
 $captchas = glob(ABSOLUTE_PATH . 'sources/captcha/captcha?????????.{jpg,jpeg,png}', GLOB_BRACE);
 $filename = $captchas[array_rand($captchas)];
@@ -23,16 +27,16 @@ list($width, $height) = getimagesize($filename);
 $pathArray = [];
 $tileId = 0;
 $combinaison = []; // Tableau qui contient le bon ordre de résolution du captcha
-for ($i=0;$i<9;$i++){
+for ($i=0;$i<$tileLength**2;$i++){
     $combinaison[] = uniqidreal(4);
 }
 
 shuffle($combinaison);
 
-for ($i = 0; $i < 3; $i++){
-    for ($j = 0; $j < 3; $j++){
-        $dst_image = imagecreatetruecolor($width / 3, $height / 3);
-        imagecopy($dst_image, $captchaImage, 0, 0, $width / 3 * $j, $height / 3 * $i, $width / 3, $height / 3);
+for ($i = 0; $i < $tileLength; $i++){
+    for ($j = 0; $j < $tileLength; $j++){
+        $dst_image = imagecreatetruecolor($width / $tileLength, $height / $tileLength);
+        imagecopy($dst_image, $captchaImage, 0, 0, $width / $tileLength * $j, $height / $tileLength * $i, $width / $tileLength, $height / $tileLength);
 
         if($extension == 'jpeg' || $extension == 'jpg') {
             imagejpeg($dst_image, ABSOLUTE_PATH . "sources/captcha/" . $imageId . "-" . $i . "_" . $j . ".jpeg");
@@ -49,25 +53,26 @@ shuffle($combinaison);
 ?>
 
 <div class="container-fluid" id="__captcha">
-        <?php
-            for ($i = 0; $i < 9; $i++){
-                if ($i % 3 == 0){
-                    echo '<div class="row">';
-                }
-        ?>
-            <div id="__captchaTile<?php  echo $i ?>" class="col-4 p-1">
-                    <img class="img-fluid float-start" src="<?php echo DOMAIN . $pathArray[$combinaison[$i]][1] ?>" alt="">
-                    <input type="hidden" 
-                            id="__tile<?php echo $i ?>"
-                            name="__tile<?php echo $i ?>"
-                            value="<?php echo $combinaison[$i] ?>">
-            </div> 
-        <?php
-                if ($i % 3 == 2){
-                    echo '</div>';
-                }
-            }
-        ?>
+    <?php
+    $cpt = 0;
+    for ($i = 0; $i < $tileLength; $i++){
+        echo '<div class="row">';
+        for ($j = 0; $j < $tileLength; $j++){
+
+
+            ?>
+            <div id="__captchaTile<?php  echo $i ?>" class="col p-1">
+                <img class="img-fluid float-start" src="<?php echo DOMAIN . $pathArray[$combinaison[$cpt]][1] ?>" alt="">
+                <input type="hidden"
+                       id="__tile<?php echo $i ?>"
+                       name="__tile<?php echo $i ?>"
+                       value="<?php echo $combinaison[$cpt] ?>">
+            </div>
+            <?php
+            $cpt++;
+        }
+    }
+    ?>
 </div>
 
 <script src="<?= DOMAIN . 'js/captcha.js'?>" crossorigin="anonymous"></script>
