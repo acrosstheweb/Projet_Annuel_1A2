6ef08f9486eb10fc16e22d6905dec487d764c096
Anaelle <aea@myges.fr>
2022-06-29 03:44:00 +0200

    fix page captchaAdmin

--- modules/user/vues/admin/captchaAdmin.php
+++ modules/user/vues/admin/captchaAdmin.php
@@ -5,35 +5,49 @@ if(!isAdmin()) {
     header('Location: ../../../../error404.php');
     die();
 }
+$title = "Fitness Essential - Modification Captcha";
+$content = "Modification Captcha";
 
 require '../../../../header.php';
 Message('captchaModify');
 ?>
 
+<div class="container-fluid d-lg-none">
+    <div class="row __profileDropdown">
+        <div class="dropdown d-grid gap-2">
+            <button class="btn dropdown-toggle text-light" type="button" id="__profileDropdownButton" data-bs-toggle="dropdown" aria-expanded="false">
+                <?= $content ?>
+            </button>
+            <ul class="dropdown-menu justify-content-center __profileDropdownMenu text-light" aria-labelledby="dropdownMenuButton1">
+                <?php include 'adminNavbar.php'; ?>
+            </ul>
+        </div>
+    </div>
+</div>
+
 <h1 class="aligned-title">Modifications captcha</h1>
+
 <div class="container-fluid">
-    <div class="row">
-        <div class="d-none col-2 d-md-flex justify-content-center">
+    <div class="row d-flex justify-content-center justify-content-lg-start">
+        <div class="d-none col-2 d-lg-flex justify-content-center">
             <?php include "adminNavbar.php"; ?>
         </div>
-        <div class="col-1"></div>
-        <div class="col-8">
-            <div class="row">
-                <div class="col-6">
-                    <h5>Changer l'image de captcha</h5>
-                    <div class="row">
-                        <form method="POST" class="col-10" action="../../scripts/admin/captchaModify.php" enctype="multipart/form-data"> <!-- Le enctype permet de spécifier que les données envoyées lors de l'envoi sont encodées lors de la soumission au serveur. -->
-                            <div>
-                                <p>Uploader une image</p>
-                                <input class="form-control" type="file" name="captcha-image" id="__captcha-image" required="required">
-                            </div><br><br>
 
-                            <input type="submit" class="btn btn-primary" required="required" name="captcha-images-form" value="Upload">
-                        </form>
-                    </div>
+        <div class="col-12 col-md-10 col-lg-8">
+            <div class="row justify-content-evenly">
+                <div class="col-12 col-md-10 col-lg-6">
+                    <h5>Changer l'image de captcha</h5>
+                    <form method="POST" class="col-10" action="../../scripts/admin/captchaModify.php" enctype="multipart/form-data"> <!-- Le enctype permet de spécifier que les données envoyées lors de l'envoi sont encodées lors de la soumission au serveur. -->
+                        <div>
+                            <p>Uploader une image</p>
+                            <input class="form-control" type="file" name="captcha-image" id="__captcha-image" required="required">
+                        </div><br><br>
+
+                        <input type="submit" class="btn btn-primary" required="required" name="captcha-images-form" value="Upload">
+                    </form>
                 </div>
 
-                <div class="col-6">
+                <div class="col-12 col-md-10 col-lg-6 mt-5 mt-lg-0">
                     <h5>Difficulté du captcha</h5>
                     <div clas="col-2">
                         <form method="POST" class="col-10" action="../../scripts/admin/captchaModify.php">
