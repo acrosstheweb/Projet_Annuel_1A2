285bb2d03fa43bda65578a49327ac718facd51a4
Wissdx <62649911+Wissdx@users.noreply.github.com>
2022-06-29 02:45:04 +0200

    Revert "REbASE"
    This reverts commit 096d9c1a11d91297f46d08fe215f65f5ccfc61e5.

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'functions.php';
-
-/*unset($_SESSION['userToken']);
-unset($_SESSION['userId']);*/
-session_destroy();
-session_start();
-setMessage('Logout', ['Vous êtes bien deconnecté'], 'success');
-header("Location: index.php");
