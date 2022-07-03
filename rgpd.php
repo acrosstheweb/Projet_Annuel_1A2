fa10acbf9c71e32d04342d1af1dd4982880aa3e1
Wissdx <62649911+Wissdx@users.noreply.github.com>
2022-07-03 23:37:06 +0200

    rgpd

---
+++ rgpd.php
@@ -0,0 +1,10 @@
+<?php
+require 'header.php';
+
+?>
+
+<marquee scrollamount="250"><h1 style="font-size:13rem;">SANANED</h1></marquee>
+<div class="d-flex justify-content-center">
+<img style="text-align:center;" src="sources/img/sananed.PNG">
+</div>
+<marquee scrollamount="150"><h1 style="font-size:10rem;">SANANED</h1></marquee>
