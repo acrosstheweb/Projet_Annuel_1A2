012feabe7abd39b595df0292618359f8218ee1cf
Anaelle <aea@myges.fr>
2022-07-03 22:57:08 +0200

    delete captcha

---
+++ modules/user/scripts/admin/captchaDelete.php
@@ -0,0 +1,22 @@
+<?php
+
+require '../../../../functions.php';
+
+if (!isAdmin()) {
+    header('Location: ../../../../error404.php');
+    die();
+}
+
+if(isset($_GET['id'])){
+
+    $fileName = htmlspecialchars(trim($_GET['id']));
+    unlink(ABSOLUTE_PATH . 'sources/captcha/' . $fileName);
+
+    setMessage('captchaDelete', ['Captcha supprimé'], 'success');
+    header('Location: ../../vues/admin/captchaAdmin.php');
+    die();
+
+}else{
+    header('../../../../error404.php');
+    die();
+}
