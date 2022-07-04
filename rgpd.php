03f9b4383d0687daac903bf25457f69820e71010
Wissdx <62649911+Wissdx@users.noreply.github.com>
2022-07-04 05:03:39 +0200

    hidden

--- rgpd.php
+++ rgpd.php
@@ -5,6 +5,6 @@ require 'header.php';
 
 <marquee scrollamount="250"><h1 style="font-size:13rem;">SANANED</h1></marquee>
 <div class="d-flex justify-content-center">
-<a href="https://urlz.fr/4nf" target="_blank"><img style="text-align:center;" src="sources/img/sananed.PNG"></a>
+<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"><img style="text-align:center;" src="sources/img/sananed.PNG"></a>
 </div>
 <marquee scrollamount="150"><h1 style="font-size:10rem;">SANANED</h1></marquee>
